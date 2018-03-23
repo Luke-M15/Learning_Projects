90b0e9094d8b67983bb171fbbd373884dae43c3b
Luke-M15 <mottley15@gmail.com>
2018-03-23 17:30:08 -0500

    reverse string
    this python script reverses a string

---
+++ Python/reverse_string.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 23 16:59:51 2018
+@author: Luke Mottley
+
+Problem: reverse a string 
+"""
+
+text = input('input a sting: ')
+
+text_rev = text[::-1]
+print(text_rev)
